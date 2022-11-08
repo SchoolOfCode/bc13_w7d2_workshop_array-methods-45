0e3b98d502ee75c76648dcfdc9b0b964be448bdd
arooj-ilyas <112420017+arooj-ilyas@users.noreply.github.com>
2022-11-08 12:30:00 +0000

    task 4 complete

--- task4.js
+++ task4.js
@@ -1,3 +1,20 @@
 let sevenTimesTable = [7, 14, 21, 28, 35, 42, 49, 56, 63, 70];
 
 let seventySevenTimesTable = [77, 154, 231, 308, 385, 461, 538, 616, 693, 770];
+
+let checkSeven = sevenTimesTable.every(function(number){
+    return number % 7 === 0;
+})
+
+let checkSeventySeven = seventySevenTimesTable.every(function(number){
+    return number % 77 === 0;
+})
+
+let findRogue = seventySevenTimesTable.find(function(number){
+    return number % 77 !== 0;
+})
+
+let findIndex = seventySevenTimesTable.findIndex(function(number){
+    return number % 77 !== 0;
+})
+console.log(checkSeven, checkSeventySeven, findRogue, findIndex)
