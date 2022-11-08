ac23a5aa2d34785e51587c37dc44f58a3b265fbe
mohamedabdi <mohamed.abdi11@hotmail.co.uk>
2022-11-08 12:21:18 +0000

    Task 2/3 done

--- task3.js
+++ task3.js
@@ -1,1 +1,19 @@
 let someNumbers = [4, 5, 6, 78, 2, 3, 45, 34, 2, 23, 5, 45, 6, 7, 23];
+
+let three = someNumbers.some(function (number) {
+  return number % 3 == 0;
+});
+
+let five = someNumbers.some(function (number) {
+  return number % 5 == 0;
+});
+
+let sixty = someNumbers.some(function (number) {
+  return number % 60 == 0;
+});
+
+let ninety = someNumbers.some(function (number) {
+  return number % 90 == 0;
+});
+
+console.log(three, five, sixty, ninety);
